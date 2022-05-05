e3f357d591a1fb6add9cd8d2abcfbe5aac304ca2
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2022-05-05 12:55:58 +0530

    golang-lint fix

--- source/iterator/cdc_iterator.go
+++ source/iterator/cdc_iterator.go
@@ -1,3 +1,18 @@
+/*
+Copyright © 2022 Meroxa, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package iterator
 
 import (
@@ -9,7 +24,7 @@ import (
 
 	"github.com/conduitio/conduit-connector-google-sheets/source/position"
 	sdk "github.com/conduitio/conduit-connector-sdk"
-	
+
 	"google.golang.org/api/option"
 	sheets "google.golang.org/api/sheets/v4"
 )
@@ -21,15 +36,15 @@ type Object struct {
 }
 
 type CDCIterator struct {
-	service *sheets.Service
-	client  *http.Client
+	service       *sheets.Service
+	client        *http.Client
 	spreadsheetID string
 	sheetID       int64
 	timeInterval  time.Duration
 	rp            position.SheetPosition
 }
 
-func NewCDCIterator(ctx context.Context, client *http.Client, spreadsheetId string, sheetId int64, interval time.Duration, pos position.SheetPosition) (*CDCIterator, error) {
+func NewCDCIterator(ctx context.Context, client *http.Client, spreadsheetID string, sheetID int64, interval time.Duration, pos position.SheetPosition) (*CDCIterator, error) {
 	// var err error
 	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
@@ -37,10 +52,10 @@ func NewCDCIterator(ctx context.Context, client *http.Client, spreadsheetId stri
 	}
 
 	c := &CDCIterator{
-		service: srv,
-		client:  client,
-		spreadsheetID: spreadsheetId,
-		sheetID:       sheetId,
+		service:       srv,
+		client:        client,
+		spreadsheetID: spreadsheetID,
+		sheetID:       sheetID,
 		timeInterval:  interval,
 		rp:            pos,
 	}
@@ -64,7 +79,7 @@ func (i *CDCIterator) Next(ctx context.Context) (sdk.Record, error) {
 	}
 
 	if len(sheetData.sheetRecords) == 0 {
-		i.rp.NextRun = time.Now().Add(i.timeInterval) //i.cfg.IterationInterval)
+		i.rp.NextRun = time.Now().Add(i.timeInterval)
 		sdk.Logger(ctx).Info().Msg(fmt.Sprintf("The next API will hit after: %v", i.rp.NextRun))
 		return sdk.Record{
 			Position: lastRowPosition.RecordPosition(),
@@ -81,8 +96,8 @@ func (i *CDCIterator) Next(ctx context.Context) (sdk.Record, error) {
 	// create the record
 	output := sdk.Record{
 		Metadata: map[string]string{
-			"SpreadsheetId": i.spreadsheetID,              //.cfg.GoogleSpreadsheetID,
-			"SheetId":       fmt.Sprintf("%d", i.sheetID), //.cfg.GoogleSheetID),
+			"SpreadsheetId": i.spreadsheetID,
+			"SheetId":       fmt.Sprintf("%d", i.sheetID),
 			"dimension":     sheetData.sheetDimension,
 		},
 		Position:  lastRowPosition.RecordPosition(),
@@ -97,11 +112,11 @@ func (i *CDCIterator) Stop() {
 	// nothing to do here
 }
 
-func fetchSheetData(ctx context.Context, srv *sheets.Service, spreadsheetId string, sheetId int64, offset int64) (*Object, error) {
+func fetchSheetData(ctx context.Context, srv *sheets.Service, spreadsheetID string, sheetID int64, offset int64) (*Object, error) {
 	var s sheets.DataFilter
 	dataFilters := []*sheets.DataFilter{}
 	s.GridRange = &sheets.GridRange{
-		SheetId:       sheetId,
+		SheetId:       sheetID,
 		StartRowIndex: offset,
 	}
 	dataFilters = append(dataFilters, &s)
@@ -112,7 +127,7 @@ func fetchSheetData(ctx context.Context, srv *sheets.Service, spreadsheetId stri
 		DataFilters:          dataFilters,
 		DateTimeRenderOption: dateTimeRenderOption,
 	}
-	res, err := srv.Spreadsheets.Values.BatchGetByDataFilter(spreadsheetId, rbt).Context(ctx).Do()
+	res, err := srv.Spreadsheets.Values.BatchGetByDataFilter(spreadsheetID, rbt).Context(ctx).Do()
 	if err != nil {
 		return nil, err
 	}
