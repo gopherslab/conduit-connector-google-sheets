06efb9e72e30b32ad03b4c68b389931d4d427a77
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2022-05-08 17:25:40 +0530

    linter fix

--- source/iterator/iterator.go
+++ source/iterator/iterator.go
@@ -1,3 +1,18 @@
+/*
+Copyright © 2022 Meroxa, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package iterator
 
 import (
@@ -197,7 +212,6 @@ func (c *SheetsIterator) getSheetRecords(ctx context.Context) ([]sdk.Record, err
 			Key:       sdk.RawData([]byte(fmt.Sprintf("A%d", index+1))),
 			Payload:   sdk.RawData(rawData),
 		})
-
 	}
 
 	fmt.Println("++++++++Successfully created the record+++++++++++")
