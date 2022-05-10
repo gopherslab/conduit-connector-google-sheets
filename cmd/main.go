87cc0add8f352e33d28016576ca5032add9b7c7a
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2022-05-10 17:38:15 +0530

    added destination connector in main

--- cmd/main.go
+++ cmd/main.go
@@ -17,10 +17,11 @@ package main
 
 import (
 	gs "github.com/conduitio/conduit-connector-google-sheets"
+	"github.com/conduitio/conduit-connector-google-sheets/destination"
 	"github.com/conduitio/conduit-connector-google-sheets/source"
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
 func main() {
-	sdk.Serve(gs.Specification, source.NewSource, nil)
+	sdk.Serve(gs.Specification, source.NewSource, destination.NewDestination)
 }
