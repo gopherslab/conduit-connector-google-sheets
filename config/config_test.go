ec895a5d3bcee2ca2b18ca3c1dd9de1710b3e38d
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2022-05-10 13:28:20 +0530

    config test lint fix

--- config/config_test.go
+++ config/config_test.go
@@ -1,17 +1,17 @@
-// Copyright © 2022 Meroxa, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* Copyright © 2022 Meroxa, Inc.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package config
 
 import (
@@ -31,27 +31,27 @@ func TestParse(t *testing.T) {
 		{
 			testCase: "Checking against if any/all required value are empty",
 			params: map[string]string{
-				"access_token":       "",
-				"refresh_token":      "",
-				"spreadsheet_id":     "",
+				"access_token":   "",
+				"refresh_token":  "",
+				"spreadsheet_id": "",
 			},
 			expected: Config{},
 		},
 		{
 			testCase: "Checking against random values case",
 			params: map[string]string{
-				"access_token":       "asdfghjkl",
-				"refresh_token":      "qweafdfv",
-				"spreadsheet_id":     "",
+				"access_token":   "asdfghjkl",
+				"refresh_token":  "qweafdfv",
+				"spreadsheet_id": "",
 			},
 			expected: Config{},
 		},
 		{
 			testCase: "Checking for IDEAL case",
 			params: map[string]string{
-				"access_token":       "access-token here",
-				"refresh_token":      "refresh-token here",
-				"spreadsheet_id":     "123abcd",
+				"access_token":   "access-token here",
+				"refresh_token":  "refresh-token here",
+				"spreadsheet_id": "123abcd",
 			},
 			expected: Config{
 				GoogleAccessToken:   "access-token here",
--- source/sourceconfig/config_test.go
+++ source/sourceconfig/config_test.go
@@ -1,3 +1,17 @@
+/* Copyright © 2022 Meroxa, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package sourceconfig
 
 import (
@@ -38,7 +52,7 @@ func TestParse(t *testing.T) {
 			},
 			expected: Config{
 				GoogleSheetID:     22,
-				IterationInterval: time.Duration(3 * time.Minute),
+				IterationInterval: 3 * time.Minute,
 			},
 		},
 		{
@@ -48,7 +62,7 @@ func TestParse(t *testing.T) {
 			},
 			expected: Config{
 				GoogleSheetID:     32,
-				IterationInterval: time.Duration(3 * time.Minute),
+				IterationInterval: 3 * time.Minute,
 			},
 		},
 		{
@@ -75,7 +89,7 @@ func TestParse(t *testing.T) {
 			},
 			expected: Config{
 				GoogleSheetID:     365,
-				IterationInterval: time.Duration(2 * time.Second),
+				IterationInterval: 2 * time.Second,
 			},
 		},
 		{
@@ -86,7 +100,7 @@ func TestParse(t *testing.T) {
 			},
 			expected: Config{
 				GoogleSheetID:     12,
-				IterationInterval: time.Duration(2 * time.Minute),
+				IterationInterval: 2 * time.Minute,
 			},
 		},
 	}
